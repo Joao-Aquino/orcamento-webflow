26b2af494a420af79e06289b0b6291a488ee156c
João Aquino <jaoaquino316@gmail.com>
2024-07-12 16:17:19 -0300

    message

--- pagina-orcamento.js
+++ pagina-orcamento.js
@@ -10,23 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
   Object.keys(quoteItems).forEach((key) => {
     const item = quoteItems[key];
     const itemDiv = document.createElement("div");
-    itemDiv.classList.add("quote-item");
-    itemDiv.innerHTML = `
+    itemDiv.classList.add("cart-item");
+    const cartItem = `
             <div class="cart-item-image-wrapper">
               <img 
-                src="${quoteItems[key]["image"]}" 
+                src="${item.image}" 
                 loading="lazy" 
                 sizes="(max-width: 991px) 
                 100vw, 60px" 
-                srcset="${quoteItems[key]["image"]} 500w, ${quoteItems[key]["image"]} 540w" 
+                srcset="${item.image} 500w, ${item.image} 540w" 
                 alt="" 
                 class="cart-item-image"
               >
             </div>
             <div class="cart-item-information">
-              <div class="cart-item-information-heading">${quoteItems[key]["name"]}</div>
-              <div class="cart-item-information-subheading">${quoteItems[key]["description"]}</div>
-              ${removeLink.outerHTML}
+              <div class="cart-item-information-heading">${item.name}</div>
+              <div class="cart-item-information-subheading">${item.description}</div>
+              <a href="#" class="cart-item-information-remove-link" data-slug="${item.slug}">Remove</a>
             </div>
             <div class="w-embed">
               <input 
@@ -38,11 +38,68 @@ document.addEventListener("DOMContentLoaded", () => {
                 oninput="validity.valid||(value='');"
                 name="quantity" 
                 autocomplete="off" 
-                value="${quoteItems[key]["quantity"]}"
-                data-slug="${quoteItems[key]["slug"]}"
+                value="${item.quantity}"
+                data-slug="${item.slug}"
               >
             </div>
-        `;
+          `;
+    itemDiv.innerHTML = cartItem;
     quoteItemsContainer.appendChild(itemDiv);
   });
+
+  // Add event listeners for remove links and quantity inputs
+  addClickEventListenersToRemoveLinks();
+  addClickEventListenersToCartItemQuantityInputs();
 });
+
+function addClickEventListenersToRemoveLinks() {
+  const removeItemLinks = Array.from(
+    document.getElementsByClassName("cart-item-information-remove-link")
+  );
+  removeItemLinks.forEach((link) => {
+    link.addEventListener("click", async (event) => {
+      const target = event.target;
+      if (target) {
+        const quoteItems = await JSON.parse(
+          localStorage?.getItem("quoteItems") || "{}"
+        );
+        if (target.dataset.slug) {
+          delete quoteItems[target.dataset.slug];
+          localStorage.setItem("quoteItems", JSON.stringify(quoteItems));
+          target.parentElement?.parentElement?.remove();
+        }
+      }
+    });
+  });
+}
+
+function addClickEventListenersToCartItemQuantityInputs() {
+  const cartQuantityInputs = Array.from(
+    document.getElementsByClassName("cart-quantity")
+  );
+  cartQuantityInputs.forEach((cartQuantityInput) => {
+    const originalCartQuantityInput = cartQuantityInput;
+    const originalCartQuantityInputValue = originalCartQuantityInput.value;
+    cartQuantityInput.addEventListener("input", async (event) => {
+      const target = event.target;
+      if (target) {
+        const quoteItems = await JSON.parse(
+          localStorage?.getItem("quoteItems") || "{}"
+        );
+        if (target.dataset.slug) {
+          if (target.value === "" || target.value == null) {
+            target.setCustomValidity("Please enter a number!");
+            target.reportValidity();
+            target.setCustomValidity("");
+            quoteItems[target.dataset.slug]["quantity"] =
+              originalCartQuantityInputValue;
+            localStorage.setItem("quoteItems", JSON.stringify(quoteItems));
+            return;
+          }
+          quoteItems[target.dataset.slug]["quantity"] = target.value;
+          localStorage.setItem("quoteItems", JSON.stringify(quoteItems));
+        }
+      }
+    });
+  });
+}
